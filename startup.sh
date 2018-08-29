7ff66e365374d4fc355799181eea38585ddd3692
StruggleYang <yq1724555319@gmail.com>
2018-08-29 15:30:38 +0800

    fix update

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ oldpid=`cat pid.txt`
 echo killing Previous pid: ${oldpid}
 kill ${oldpid}
 echo runing app
-sh agentClient $1 >> ./logs/agent_clinet.log 2>&1 &
+sh agentClient $1 > /dev/null &
 echo "$!" > pid.txt
 newpid=`cat pid.txt`
 echo runing new app pid is : ${newpid}
