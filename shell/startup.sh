fcb6cc2f2f70e93c3ef36bef5f99ba82eca63748
StruggleYang <1724555319@qq.com>
2018-09-30 16:15:04 +0800

    fix

--- shell/startup.sh
+++ shell/startup.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+if [ $# -lt 1 ];then
+        echo "connect serverUrl not set ,Please set e.g. [http://127.0.0.1:6886]"
+        exit 1
+    fi
 oldpid=`cat pid.txt`
 echo killing Previous pid: ${oldpid}
 kill ${oldpid}
